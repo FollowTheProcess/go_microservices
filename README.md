c0e78ebc81ef1b96b65cad0ebaf4ad3af08e7877
Tom Fleet <tomfleet2018@gmail.com>
2021-09-04 11:28:00 +0100

    End of Ep1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # go_microservices
+
+This is the repo containing my following along of the [Building Microservices with Go] series
+
+[Building Microservices with Go]: https://youtube.com/playlist?list=PLmD8u-IFdreyh6EUfevBcbiuCKzFk0EW_
--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Hello World")
+		data, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Oops", http.StatusBadRequest)
+			return
+		}
+		defer r.Body.Close()
+		fmt.Fprintf(w, "Data: %s\n", data)
+	})
+
+	http.HandleFunc("/goodbye", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Goodbye World")
+	})
+
+	log.Fatal(http.ListenAndServe(":9090", nil))
 }
